c69159eefec91a970fdaf9d6ed5a3d988bfc8dd2
MD Akmami <akhmami@gmail.com>
2022-11-11 14:06:57 +0700

    qr code scanner fixed

--- assets/qr-scanner.js
+++ assets/qr-scanner.js
@@ -14,9 +14,8 @@ qrcode.callback = res => {
     if (res) {
         // outputData.innerText = res;
         scanning = false;
-
         const arr = res.split('&');
-        console.log(arr.length);
+
         if (arr.length == 2) {
             var formdata = new FormData();
             formdata.append("no_pendaftaran", arr[0]);
